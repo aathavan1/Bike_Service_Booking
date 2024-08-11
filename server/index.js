5e4b4767464a4795907b49aaf59953ae3290db10
Aathavan G <aathavan972004@gmail.com>
2024-08-11 09:05:16 +0530

    a

--- server/index.js
+++ server/index.js
@@ -5,9 +5,9 @@ const app = express()
 app.use(cors(
 
     {
-    //     origin: ["https://bike-service-frontend-six.vercel.app/"],
-        methods: ["POST", "GET","PUT","DELETE"]
-    //     credentials: true
+        origin: ["https://bike-service-frontend-six.vercel.app/"],
+        methods: ["POST", "GET","PUT","DELETE"],
+        credentials: true
     }
 ))
 app.use(express.json())
